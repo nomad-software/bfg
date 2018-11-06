e3ab0035926ba84956baa5592534d4fcef361b01
Gary Willoughby <dev@nomad.so>
2018-11-06 21:02:32 +0000

    Reordered code and added token constants.

--- bfg.go
+++ bfg.go
@@ -7,77 +7,17 @@ import (
 	"os"
 )
 
-func main() {
-
-	if len(os.Args) <= 1 {
-		fmt.Println("No program file argument")
-		return
-	}
-
-	program, _ := ioutil.ReadFile(os.Args[1])
-
-	input := bufio.NewReader(os.Stdin)
-	output := bufio.NewWriter(os.Stdout)
-	defer output.Flush()
-
-	stack := make([]byte, 30720)
-	cell := 0
-	loops := make([]int, 2056)
-	loop := -1
-	skip := 0
-
-	ops := parseOperators(program)
-
-	for x := 0; x < len(ops); x++ {
-
-		switch ops[x].token {
-
-		case '>':
-			cell += ops[x].count
-
-		case '<':
-			cell -= ops[x].count
-
-		case '+':
-			stack[cell] += byte(ops[x].count)
-
-		case '-':
-			stack[cell] -= byte(ops[x].count)
-
-		case '.':
-			output.WriteByte(stack[cell])
-			if stack[cell] == '\n' {
-				output.Flush()
-			}
-
-		case ',':
-			stack[cell], _ = input.ReadByte()
-
-		case '[':
-			if stack[cell] == 0 {
-				skip++
-				for skip > 0 {
-					x++
-					if ops[x].token == '[' {
-						skip++
-					} else if ops[x].token == ']' {
-						skip--
-					}
-				}
-			} else {
-				loop++
-				loops[loop] = x
-			}
-
-		case ']':
-			if stack[cell] == 0 {
-				loop--
-			} else {
-				x = loops[loop]
-			}
-		}
-	}
-}
+// Brainfuck operators.
+const (
+	right = '>'
+	left  = '<'
+	add   = '+'
+	sub   = '-'
+	in    = ','
+	out   = '.'
+	open  = '['
+	close = ']'
+)
 
 // Operator is a struct holding a parsed operator and how many times it's used
 // consecutively.
@@ -97,13 +37,13 @@ func parseOperators(program []byte) []operator {
 
 		switch op {
 
-		case '>':
+		case right:
 			fallthrough
-		case '<':
+		case left:
 			fallthrough
-		case '+':
+		case add:
 			fallthrough
-		case '-':
+		case sub:
 			if op == current.token {
 				current.count++
 				break
@@ -115,13 +55,13 @@ func parseOperators(program []byte) []operator {
 			current = operator{op, 1}
 			break
 
-		case '.':
+		case in:
 			fallthrough
-		case ',':
+		case out:
 			fallthrough
-		case '[':
+		case open:
 			fallthrough
-		case ']':
+		case close:
 			ops = append(ops, current)
 			current = operator{op, 1}
 			break
@@ -130,4 +70,77 @@ func parseOperators(program []byte) []operator {
 
 	ops = append(ops, current)
 	return ops
+}
+
+// Main entry point.
+func main() {
+
+	if len(os.Args) <= 1 {
+		fmt.Println("No program file argument")
+		return
+	}
+
+	program, _ := ioutil.ReadFile(os.Args[1])
+
+	input := bufio.NewReader(os.Stdin)
+	output := bufio.NewWriter(os.Stdout)
+	defer output.Flush()
+
+	stack := make([]byte, 30720)
+	cell := 0
+	loops := make([]int, 2056)
+	start := -1
+	skip := 0
+
+	ops := parseOperators(program)
+
+	for x := 0; x < len(ops); x++ {
+
+		switch ops[x].token {
+
+		case right:
+			cell += ops[x].count
+
+		case left:
+			cell -= ops[x].count
+
+		case add:
+			stack[cell] += byte(ops[x].count)
+
+		case sub:
+			stack[cell] -= byte(ops[x].count)
+
+		case in:
+			stack[cell], _ = input.ReadByte()
+
+		case out:
+			output.WriteByte(stack[cell])
+			if stack[cell] == '\n' {
+				output.Flush()
+			}
+
+		case open:
+			if stack[cell] == 0 {
+				skip++
+				for skip > 0 {
+					x++
+					if ops[x].token == '[' {
+						skip++
+					} else if ops[x].token == ']' {
+						skip--
+					}
+				}
+			} else {
+				start++
+				loops[start] = x
+			}
+
+		case close:
+			if stack[cell] == 0 {
+				start--
+			} else {
+				x = loops[start]
+			}
+		}
+	}
 }
