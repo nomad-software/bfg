bd07dd77f23b0dc4fa11fea82a768e7b25600e33
Gary Willoughby <dev@nomad.uk.net>
2019-08-11 10:53:46 +0100

    Changed register names in the linux/amd64 compiler.

--- nasm/compile_linux_amd64.go
+++ nasm/compile_linux_amd64.go
@@ -19,13 +19,13 @@ func newAssembly(tokens []token.Token) nasm {
 	asm.write("global _start")
 	asm.write("section .text")
 	asm.write("_start:")
-	asm.write("mov rsp, stack")
+	asm.write("mov r8, stack")
 
 	// This write seems unnecessary but it's required or else subsequent reads from
 	// stdin fail. I need to investigate why.
 	asm.write("mov rax, 1")
 	asm.write("mov rdi, 1")
-	asm.write("mov rsi, rsp")
+	asm.write("mov rsi, r8")
 	asm.write("mov rdx, 1")
 	asm.write("syscall")
 
@@ -33,42 +33,42 @@ func newAssembly(tokens []token.Token) nasm {
 		switch t.Type {
 
 		case token.RightType:
-			asm.write("add rsp, %d", t.Shift)
+			asm.write("add r8, %d", t.Shift)
 
 		case token.LeftType:
-			asm.write("sub rsp, %d", t.Shift)
+			asm.write("sub r8, %d", t.Shift)
 
 		case token.AddType:
-			asm.write("add byte [rsp], %d", t.Value)
+			asm.write("add byte [r8], %d", t.Value)
 
 		case token.SubType:
-			asm.write("sub byte [rsp], %d", t.Value)
+			asm.write("sub byte [r8], %d", t.Value)
 
 		case token.InType:
 			asm.write("mov rax, 0")
 			asm.write("mov rdi, 0")
-			asm.write("mov rsi, rsp")
+			asm.write("mov rsi, r8")
 			asm.write("syscall")
 
 		case token.OutType:
 			asm.write("mov rax, 1")
 			asm.write("mov rdi, 1")
-			asm.write("mov rsi, rsp")
+			asm.write("mov rsi, r8")
 			asm.write("mov rdx, 1")
 			asm.write("syscall")
 
 		case token.OpenType:
-			asm.write("cmp byte [rsp], 0")
+			asm.write("cmp byte [r8], 0")
 			asm.write("je close_loop_%d", t.Jump)
 			asm.write("open_loop_%d:", i)
 
 		case token.CloseType:
-			asm.write("cmp byte [rsp], 0")
+			asm.write("cmp byte [r8], 0")
 			asm.write("jne open_loop_%d", t.Jump)
 			asm.write("close_loop_%d:", i)
 
 		case token.ZeroType:
-			asm.write("mov byte [rsp], 0")
+			asm.write("mov byte [r8], 0")
 		}
 	}
 
