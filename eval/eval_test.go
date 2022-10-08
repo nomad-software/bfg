bc063cec0563807a8140abe93d26e132b85bbc50
Gary Willoughby <dev@nomad.uk.net>
2022-10-08 19:17:59 +0100

    Added other benchmarks

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -11,13 +11,63 @@ import (
 	"github.com/nomad-software/bfg/lexer"
 )
 
-func BenchmarkEvaluator(b *testing.B) {
+func BenchmarkMandelbrot(b *testing.B) {
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	program, err := os.ReadFile(path.Join(wd, "../programs/mandelbrot.bf"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	input := bufio.NewReader(os.Stdin)
+	output := bufio.NewWriter(io.Discard)
+	defer output.Flush()
+
+	tokens := lexer.New(program).Tokens
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for x := 0; x < b.N; x++ {
+		Evaluate(tokens, input, output)
+	}
+}
+
+func BenchmarkHanoi(b *testing.B) {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	program, err := os.ReadFile(path.Join(wd, "../programs/hanoi.bf"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	input := bufio.NewReader(os.Stdin)
+	output := bufio.NewWriter(io.Discard)
+	defer output.Flush()
+
+	tokens := lexer.New(program).Tokens
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for x := 0; x < b.N; x++ {
+		Evaluate(tokens, input, output)
+	}
+}
+
+func BenchmarkLong(b *testing.B) {
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	program, err := os.ReadFile(path.Join(wd, "../programs/long.bf"))
 	if err != nil {
 		log.Fatalln(err)
 	}
